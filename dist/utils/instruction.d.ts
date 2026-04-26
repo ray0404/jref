f11af24f25f24200bbee66192715a0f2540bac2f
ray0404 <rayvalentinmusic@gmail.com>
2026-04-26 10:21:17 -0600

    chore: removed irrelevant files

--- dist/utils/instruction.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Automatically generates a project instruction based on the contents of the root directory.
- */
-export declare function generateInstruction(rootDir: string): Promise<string>;
-//# sourceMappingURL=instruction.d.ts.map
